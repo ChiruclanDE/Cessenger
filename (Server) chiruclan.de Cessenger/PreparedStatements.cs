42dc6f415ec93ff7d5c00c0530e9f1748ea03eda
Chiruclan <dev@chiruclan.de>
2013-10-26 09:31:29 +0200

    added check_login_id

--- (Server) chiruclan.de Cessenger/PreparedStatements.cs
+++ (Server) chiruclan.de Cessenger/PreparedStatements.cs
@@ -3,5 +3,6 @@
     public struct _prepared
     {
         public string get_account_id = "SELECT `id` FROM `accounts` WHERE `username` = ?";
+        public string check_login_id = "SELECT `id` FROM `accounts` WHERE `username` = ? AND `sha1_pass_hash` = ?";
     }
 }
